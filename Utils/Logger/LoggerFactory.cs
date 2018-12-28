6f9e2b2708ba52d57c317d553c75f37d8fd03a30
Pawel Kubiak <pawel.kubiak@ormlondon.com>
2018-12-28 18:36:43 +0100

    Implemented LoggerFactory

---
+++ Utils/Logger/LoggerFactory.cs
@@ -0,0 +1,74 @@
+﻿using Amazon;
+using Amazon.CloudWatchLogs;
+using Amazon.Runtime;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.AwsCloudWatch;
+using Serilog.Sinks.Elasticsearch;
+using System;
+
+namespace Utils.Logger
+{
+    public static class LoggerFactory
+    {
+        public static LoggerConfiguration BuildEmptyLoggerConfiguration()
+        {
+            return new LoggerConfiguration();
+        }
+
+        public static ILogger CreateLogger(LoggerConfiguration loggerConfiguration, LogEventLevel minimumLogEventLevel)
+        {
+            return loggerConfiguration
+                .MinimumLevel.Is(minimumLogEventLevel)
+                .CreateLogger();
+        }
+
+        public static LoggerConfiguration AppendFileLogger(this LoggerConfiguration logger, string logFilePath, int retainedFileCountLimit = 14)
+        {
+            return logger
+                .WriteTo.RollingFile(pathFormat: logFilePath, formatter: new CompactJsonFormatter(), retainedFileCountLimit: retainedFileCountLimit);
+        }
+
+        public static LoggerConfiguration AppendConsoleLogger(this LoggerConfiguration logger)
+        {
+            return logger
+                .WriteTo.Console(formatter: new CompactJsonFormatter());
+        }
+
+        public static LoggerConfiguration AppendAwsCloudwatchLogger(this LoggerConfiguration logger, string logGroupName, LogEventLevel minimumLogEvent, string accessKey, string securityKey)
+        {
+            // customer formatter
+            var formatter = new CompactJsonFormatter();
+
+            // options for the sink defaults in https://github.com/Cimpress-MCP/serilog-sinks-awscloudwatch/blob/master/src/Serilog.Sinks.AwsCloudWatch/CloudWatchSinkOptions.cs
+            var options = new CloudWatchSinkOptions
+            {
+                LogGroupName = logGroupName,
+                TextFormatter = formatter,
+                MinimumLogEventLevel = minimumLogEvent,
+                LogStreamNameProvider = new DefaultLogStreamProvider(),
+                RetryAttempts = 5
+            };
+
+            var credentials = new BasicAWSCredentials(accessKey, securityKey);
+
+            // setup AWS CloudWatch client
+            var client = new AmazonCloudWatchLogsClient(credentials, RegionEndpoint.EUWest1);
+
+            return logger
+                .WriteTo.AmazonCloudWatch(options, client);
+        }
+
+        public static LoggerConfiguration AppendElasticsearchLogger(this LoggerConfiguration logger, string uri, string indexName)
+        {
+            var options = new ElasticsearchSinkOptions(new Uri(uri))
+            {
+                IndexFormat = $"{indexName}-{0:yyyy.MM}",
+            };
+
+            return logger
+                .WriteTo.Elasticsearch(options);
+        }
+    }
+}
---
+++ Utils/Services/FuzzyMatchingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Utils
+{
+    public static class FuzzyMatchingService
+    {
+
+    }
+}
